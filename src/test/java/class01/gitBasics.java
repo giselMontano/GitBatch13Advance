c79f65dfe48720c37e756b6f98fe06e5e6db7219
giselMontano <giselalesita@gmail.com>
2022-08-28 18:18:55 -0400

    temperature check

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class gitBasics {
+    //next day assignment
+    //CHECK TEMPERATURE AND PRINT OUT IF IS A GOOD DAY
+
+    public static void main(String[] args) {
+        System.out.println("It is a beautiful day");
+    }
+}
